cf9495aee9b265b18681bde58d75094b75c1f353
jenpin <jennifer2pinto@yahoo.co.in>
2017-12-11 10:02:43 +1100

    first commit

---
+++ src/main/java/com/au/entry/EntryPoint.java
@@ -0,0 +1,10 @@
+package com.au.entry;
+
+/**
+ * Created by jpinto on 11/12/2017.
+ */
+public class EntryPoint {
+    public static void main ( String[] args ) {
+        System.out.println ("hi" );
+    }
+}
